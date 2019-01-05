0c20249a7636b6a0936d470c2632e8fcd4593c05
Random6554 <39579011+Random6554@users.noreply.github.com>
2019-01-05 14:11:31 +0000

    Update stop-iptables.sh

--- Debian/stop-iptables.sh
+++ Debian/stop-iptables.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# https://www.cyberciti.biz/faq/ubuntu-start-stop-iptables-service/
 # Linux Firewall: Simple Shell Script To Stop and Flush All Iptables Rules
 # Some Linux distros like Debian do not have /etc/init.d/iptables stop script
 # This can be also called from cron job if you are testing new firewall on
